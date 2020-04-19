1e8e08a31bf398c98b38d2bece875c085a477e79
yaoxh6 <2879813862@qq.com>
2020-04-19 21:21:05 +0800

    first

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+
+    public GameObject model;
+    public PlayerInput playerInput;
+    public float walkSpeed = 2.0f;
+    public float runMultiplier = 2.7f;
+
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rigidbody;
+    private Vector3 movingVec;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("forward", playerInput.Dmag * Mathf.Lerp(anim.GetFloat("forward"), (playerInput.bIsRun ? 2.0f : 1.0f), 0.5f));
+        if(playerInput.jump)
+        {
+            anim.SetTrigger("jump");
+        }
+        if (playerInput.Dmag > 0.1f)
+        {
+            model.transform.forward = Vector3.Slerp(model.transform.forward, playerInput.Dvec, 0.3f);
+        }
+        movingVec = model.transform.forward * playerInput.Dmag * walkSpeed * (playerInput.bIsRun ? runMultiplier : 1.0f);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.position += movingVec * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [Header("----- key settings -----")]
+    public string KeyUp;
+    public string KeyDown;
+    public string KeyLeft;
+    public string KeyRight;
+
+    public string keyA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("----- Output signals -----")]
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    public bool bIsRun;
+
+    public bool jump;
+    private bool lastjump;
+
+    [Header("----- others -----")]
+    public bool inputEnabled;
+
+    private float TargetDup;
+    private float TargetDright;
+    private float VelocityDup;
+    private float VelocityRight;
+
+
+
+    void Start()
+    {
+        KeyUp = "w";
+        KeyDown = "s";
+        KeyLeft = "a";
+        KeyRight = "d";
+
+        inputEnabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetDup = (Input.GetKey(KeyUp) ? 1.0f : 0.0f) - (Input.GetKey(KeyDown) ? 1.0f : 0.0f);
+        TargetDright = (Input.GetKey(KeyRight) ? 1.0f : 0.0f) - (Input.GetKey(KeyLeft) ? 1.0f : 0.0f);
+
+        if(inputEnabled == false)
+        {
+            TargetDup = 0;
+            TargetDright = 0;
+        } 
+
+
+        Dup = Mathf.SmoothDamp(Dup, TargetDup, ref VelocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, TargetDright, ref VelocityRight, 0.1f);
+
+
+        Vector2 tempDAxis = SquareToCircle(new Vector2(Dright, Dup));
+        float Dright2 = tempDAxis.x;
+        float Dup2 = tempDAxis.y;
+
+        Dmag = Mathf.Sqrt(Mathf.Sqrt(Dup2 * Dup2 + Dright2 * Dright2));
+        Dvec = Dup * transform.forward + Dright * transform.right;
+
+        bIsRun = Input.GetKey(keyA);
+
+        bool newJump = Input.GetKey(keyB);
+        if(newJump != lastjump && newJump == true)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+        lastjump = newJump;
+    }
+
+    Vector2 SquareToCircle(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+        return output;
+    }
+}
